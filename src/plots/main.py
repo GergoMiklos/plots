0ae04032bc32fe054b7d4a40ad990db3237b67ad
Gergo Miklos <gergo.marcell.miklos@gmail.com>
2022-05-01 00:28:01 +0200

    Phase 4.0 works and looks nice

--- src/plots/main.py
+++ src/plots/main.py
@@ -1,8 +1,6 @@
-import asyncio
 import json
 import sys
 import threading
-import time
 import types
 from typing import Optional, Any
 from uuid import uuid4
@@ -11,171 +9,159 @@ import nbformat
 import tornado.web
 import tornado.websocket
 import tornado.ioloop
-from tornado import httputil
-from tornado.platform.asyncio import AnyThreadEventLoopPolicy
-from tornado.queues import Queue
 
-from src.plots.run_context import ScriptRunContext, set_script_run_context
+from src.plots.run_context import RunContext, set_run_context, ScriptInfo, SessionState
 
-# import asyncio todo: we need different loops in different threads, otherwise everything will be execed on the main
-# from tornado.platform.asyncio import AnyThreadEventLoopPolicy
-# asyncio.set_event_loop_policy(AnyThreadEventLoopPolicy())
+# todo: locking states, threadpool, cleanup
 
 ioloop = tornado.ioloop.IOLoop.current()
 
 
-# TODO !!! : https://www.tornadoweb.org/en/stable/web.html#thread-safety-notes
-
-# todo: locking states, threadpool, cleanup
-
-class WsHandler(tornado.websocket.WebSocketHandler):
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
 
     async def open(self):
         print("New client connected")
         await self.write_message("You are connected")
-        self.read_queue = Queue(1)
-        self.singleton = Singleton.get_current()
-        self.singleton.open(self)
+        # self.read_queue = Queue(1)
+        server = Server.get_current()
+        server.open(self)
 
-    async def on_message(self, message):
-        # self.singleton.on_message(message)
-        print('Putting message:', message)
-        await self.read_queue.put(message)
+    # async def on_message(self, message):
+    #     print(message)
 
-    async def read_message(self):
-        print('Waiting message...')
-        msg = await self.read_queue.get()
-        print('Got message:', msg)
-        return msg
+    # async def on_message(self, message):
+    #     # self.singleton.on_message(message)
+    #     print('Putting message:', message)
+    #     await self.read_queue.put(message)
+    #
+    # async def read_message(self):
+    #     print('Waiting message...')
+    #     msg = await self.read_queue.get()
+    #     print('Got message:', msg)
+    #     return msg
 
-    def on_close(self):
-        print("Client disconnected")
+    # def on_close(self):
+    #     print("Client disconnected")
 
     def check_origin(self, origin):
         return True
 
 
-class Singleton:  # todo not sure about that this should be singleton
-    # todo  shuold be: store the collection of script runners, nothing else
-    _singleton: Optional["Singleton"] = None
+class Server:
+    _singleton: Optional["Server"] = None
 
     @classmethod
-    def get_current(cls) -> "Singleton":
-        if Singleton._singleton is None:
-            Singleton._singleton = Singleton()
-
-        return Singleton._singleton
+    def get_current(cls) -> "Server":
+        if Server._singleton is None:
+            Server._singleton = Server()
+        return Server._singleton
 
     def __init__(self):
-        if Singleton._singleton is not None:
-            raise RuntimeError("Singleton already initialized. Use .get_current() instead")
-        Singleton._singleton = self
-        self.ws: WsHandler = None
-        self.sr = None
-        self.periodic = None
+        if Server._singleton is not None:
+            raise RuntimeError("Singleton already initialized. Call 'get_current' instead")
+        Server._singleton = self
 
-    def open(self, ws):
-        self.ws = ws
-        self.sr = ScriptRunner(ScriptRunContext('../../nb_script.ipynb'))
-        # todo use server instead of ws, so it can contain logic (like blocking output on rerun)
-        self.ws.on_message = self.sr.rerun
-        self.periodic = tornado.ioloop.PeriodicCallback(self.output, callback_time=100, jitter=0.1)
-        ioloop.spawn_callback(self.listening)
-        self.periodic.start()
+        self.scripts = {}
+        self.sessions = {}
 
-    def output(self):
-        if len(self.sr.ctx.output) > 0:
-            self.write_message(self.sr.ctx.output.pop(0))
+    def open(self, websocket: WebSocketHandler):
+        script_info = ScriptInfo('../../nb_script.ipynb')  # todo get by url + create if missing
+        session_id = uuid4()
 
-    async def listening(self):
-        print('listening...')
-        while True:
-            e = await self.sr.ctx.q.get()
-            self.write_message(e)
+        websocket.on_close = lambda _: self.close(session_id)
 
-    def write_message(self, message):
-        self.ws.write_message(">>> " + message)
+        self.sessions[session_id] = SessionHandler(session_id, websocket, ioloop, script_info)
+
+    def close(self, session_id):
+        self.sessions[session_id].close()
+        del self.sessions[session_id]
+
+
+class SessionHandler:  # session based handler for messages and everything
+    def __init__(self, session_id, websocket, ioloop, script_info: ScriptInfo):
+        self.session_id = session_id
+        # Tornado web server is available only from the main thread,
+        # which can be reached by executing a callback through the main ioloop.
+        # See: https://www.tornadoweb.org/en/stable/web.html#thread-safety-notes
+        self.ioloop = ioloop
+
+        self.websocket = websocket
+        self.websocket.on_message = self._on_input
+
+        self.script_runner = ScriptRunner(
+            run_context=RunContext(write_output=self._write_output),
+            script_info=script_info
+        )
+        self.script_runner.run()
+
+    def _on_input(self, message):
+        print('input:', message)
+        self.script_runner.run()
+
+    def _write_output(self, message):
+        ioloop.add_callback(self.websocket.write_message, message)
+
+    def close(self):
+        print('Session closed!')
 
 
 class ScriptRunner:
-    def __init__(self, ctx: ScriptRunContext = None):
-        self.script_path = ctx.script_path
+    def __init__(self, run_context: RunContext, script_info: ScriptInfo):
+        self.run_context = run_context
+        self.script_info = script_info
+        self.script_module = None
         self.script_thread = None
-        self.ctx = ctx
-
-    def rerun(self, msg):
-        if self.script_thread and self.script_thread.is_alive():
-            self.ctx.data = 'rerun: ' + msg
-        self.run()
 
     def run(self):
         print('Starting thread...')
         self.script_thread = threading.Thread(
-            target=self._run_script_thread,
-            name="ScriptRunner:" + self.script_path,
-            args=[self.ctx]
+            target=self._run_script,
+            name="ScriptRunner:" + self.script_info.name,
         )
         self.script_thread.start()
-        self.script_thread.join()  # todo not needed?
-        print('Ctx data after run:', json.dumps(self.ctx.data, indent=4))
 
-    def _run_script_thread(self, ctx):  # todo we should do an infinite thread
-        print('Running thread...')
-        set_script_run_context(ctx)
-        self.nb_runner = NotebookRunner(ctx.script_path)
-        self.nb_runner.run()
+        self.script_thread.join()  # todo only for debug
+        print_module(self.script_module)
 
+    def _run_script(self):
+        set_run_context(self.run_context)
 
-class NotebookRunner:
-    """
-    Todo: what is the difference between:
-    - [ nbcleint: https://nbclient.readthedocs.io/en/latest/reference/nbclient.html ] does not fit to reqs
-    - IPython InteractiveShell: https://ipython.readthedocs.io/en/stable/api/generated/IPython.core.interactiveshell.html
-    """
-
-    def __init__(self, path):
-        self.module = None
-        self.path = path
-
-    def run(self,):
-        print('Running notebook...')
-        with open(self.path, "r", encoding="utf-8") as file:
+        with open(self.script_info.script_path, "r", encoding="utf-8") as file:
             notebook = nbformat.read(file, 4)
 
-        if not self.module:
+        if not self.script_module:
             module = types.ModuleType("__main__")
-            module.__file__ = self.path
+            module.__file__ = self.script_info.script_path
             module.__loader__ = self
             sys.modules["__main__"] = module
-            self.module = module
+            self.script_module = module
 
+        print('Running cells...')
         for cell in notebook.cells:
             if cell.cell_type == 'code':
-                exec(cell.source, self.module.__dict__)  # todo transform cell with IPython
-
-        print_module(self.module.__dict__)
-
-    def run_cell(self, cell_i=None):
-        print('Running cell...')
-        with open(self.path, "r", encoding="utf-8") as file:
-            notebook = nbformat.read(file, 4)
-
-        cell = [cell for cell in notebook.cells if  cell.cell_type == 'code'][cell_i]
-        exec(cell.source, self.module.__dict__)  # todo transform cell with IPython
-
-        print_module(self.module.__dict__)
+                exec(cell.source, self.script_module.__dict__)  # todo transform cell with IPython, compile
 
 
-def get_filename(path):
-    return path.split('/\\')[-1].replace('.ipynb', '')
+# class NotebookRunner:
+#     """ Investigate:
+#   nbclient: https://nbclient.readthedocs.io/en/latest/reference/nbclient.html
+#   IPython InteractiveShell: https://ipython.readthedocs.io/en/stable/api/generated/IPython.core.interactiveshell.html
+#     """
+
 
 def print_module(module):
-    print(json.dumps(module.__dict__))
+    import pprint
+    pp = pprint.PrettyPrinter(depth=4)
+    builtins = ['__name__', '__doc__', '__package__', '__loader__', '__spec__', '__file__', '__builtins__']
+    user_data = {key: module.__dict__[key] for key in module.__dict__ if key not in builtins}
+    pp.pprint(f"{user_data=}")
 
 
 application = tornado.web.Application([
-    (r"/", WsHandler),
+    (r"/", WebSocketHandler),
 ])
-application.listen(8888)
+port = 8888
+application.listen(port)
+print('Application started listening on port', port)
 
 ioloop.start()
