5fa92959a140be482ed1a9b4e6ac46629585542f
Gergo Miklos <gergo.marcell.miklos@gmail.com>
2022-05-01 14:26:14 +0200

    Phase 4.1 works and handle text input

--- src/plots/main.py
+++ src/plots/main.py
@@ -2,7 +2,7 @@ import json
 import sys
 import threading
 import types
-from typing import Optional, Any
+from typing import Optional, Any, Union, Awaitable
 from uuid import uuid4
 
 import nbformat
@@ -12,36 +12,20 @@ import tornado.ioloop
 
 from src.plots.run_context import RunContext, set_run_context, ScriptInfo, SessionState
 
-# todo: locking states, threadpool, cleanup
 
-ioloop = tornado.ioloop.IOLoop.current()
+# todo: locking states, threadpool, cleanup
 
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
 
+    async def on_message(self, message: Union[str, bytes]):
+        pass
+
     async def open(self):
         print("New client connected")
         await self.write_message("You are connected")
-        # self.read_queue = Queue(1)
         server = Server.get_current()
         server.open(self)
-
-    # async def on_message(self, message):
-    #     print(message)
-
-    # async def on_message(self, message):
-    #     # self.singleton.on_message(message)
-    #     print('Putting message:', message)
-    #     await self.read_queue.put(message)
-    #
-    # async def read_message(self):
-    #     print('Waiting message...')
-    #     msg = await self.read_queue.get()
-    #     print('Got message:', msg)
-    #     return msg
-
-    # def on_close(self):
-    #     print("Client disconnected")
 
     def check_origin(self, origin):
         return True
@@ -61,16 +45,33 @@ class Server:
             raise RuntimeError("Singleton already initialized. Call 'get_current' instead")
         Server._singleton = self
 
+        self.ioloop = None
+        self.tornado_web_application = None
+
         self.scripts = {}
         self.sessions = {}
+
+    def start(self, port=8888):
+        """
+        Starts the tornado web application
+        """
+        self.ioloop = tornado.ioloop.IOLoop.current()
+
+        self.tornado_web_application = tornado.web.Application([
+            (r"/ws", WebSocketHandler),
+        ])
+        self.tornado_web_application.listen(port)
+        print('Application started listening on port', port)
+
+        self.ioloop.start()
 
     def open(self, websocket: WebSocketHandler):
         script_info = ScriptInfo('../../nb_script.ipynb')  # todo get by url + create if missing
         session_id = uuid4()
 
-        websocket.on_close = lambda _: self.close(session_id)
+        websocket.on_close = lambda: self.close(session_id)
 
-        self.sessions[session_id] = SessionHandler(session_id, websocket, ioloop, script_info)
+        self.sessions[session_id] = SessionHandler(session_id, websocket, self.ioloop, script_info)
 
     def close(self, session_id):
         self.sessions[session_id].close()
@@ -85,24 +86,38 @@ class SessionHandler:  # session based handler for messages and everything
         # See: https://www.tornadoweb.org/en/stable/web.html#thread-safety-notes
         self.ioloop = ioloop
 
+        self.input_state = {}
+
         self.websocket = websocket
         self.websocket.on_message = self._on_input
 
         self.script_runner = ScriptRunner(
-            run_context=RunContext(write_output=self._write_output),
+            run_context=RunContext(write_output=self._write_output, input_state=self.input_state),
             script_info=script_info
         )
         self.script_runner.run()
 
-    def _on_input(self, message):
+    async def _on_input(self, message):
+        """
+        Receiving inputs from the client, given by its input key.
+        If the input value changed, reruns the script with the new value
+        """
         print('input:', message)
-        self.script_runner.run()
+        msg_dict = json.loads(message)
+        # FIXME: investigate locking strategies for input state
+        if not self.input_state.get(msg_dict['key']) == msg_dict['data']['value']:
+            self.input_state[msg_dict['key']] = msg_dict['data']['value']
+            self.script_runner.run()
 
     def _write_output(self, message):
-        ioloop.add_callback(self.websocket.write_message, message)
+        """
+        Sending output to the client through the websocket
+        """
+        # Using ioloop callback, because Tornado web server is available only from the main thread
+        self.ioloop.add_callback(self.websocket.write_message, message)
 
     def close(self):
-        print('Session closed!')
+        print('Session closed')
 
 
 class ScriptRunner:
@@ -120,7 +135,7 @@ class ScriptRunner:
         )
         self.script_thread.start()
 
-        self.script_thread.join()  # todo only for debug
+        self.script_thread.join()  # FIXME: only for debug
         print_module(self.script_module)
 
     def _run_script(self):
@@ -139,7 +154,7 @@ class ScriptRunner:
         print('Running cells...')
         for cell in notebook.cells:
             if cell.cell_type == 'code':
-                exec(cell.source, self.script_module.__dict__)  # todo transform cell with IPython, compile
+                exec(cell.source, self.script_module.__dict__)  # TODO: transform cell with IPython, compile
 
 
 # class NotebookRunner:
@@ -157,11 +172,5 @@ def print_module(module):
     pp.pprint(f"{user_data=}")
 
 
-application = tornado.web.Application([
-    (r"/", WebSocketHandler),
-])
-port = 8888
-application.listen(port)
-print('Application started listening on port', port)
-
-ioloop.start()
+server = Server.get_current()
+server.start()
